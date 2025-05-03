973caf930d0f18708a216698d4b649a7455b4442
Osamu Tsuzuki <regulus@OsamunoMac-mini.local>
2025-05-03 10:43:13 +0900

    fix: remove fallback port for Render

--- app.py
+++ app.py
@@ -2046,7 +2046,6 @@ def dynamic_template(template_name):
 def index():
     return render_template('index.html')
 
-
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 10000))
+    port = int(os.environ["PORT"])
     app.run(host="0.0.0.0", port=port)
